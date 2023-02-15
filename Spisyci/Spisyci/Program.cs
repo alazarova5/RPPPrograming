c4834143a1fe8eae3c3f5149b50f252a1fecaee7
alazarova5 <aleksandralazarova25@gmail.com>
2023-02-15 09:30:27 +0200

    zad1

---
+++ Spisyci/Spisyci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RemoveNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //list of intiger numbers
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            //finding last element
+            int last = nums.Last();
+
+            //removing all like the last element
+            nums.RemoveAll(x => x == last); 
+
+            //print
+            Console.WriteLine(string.Join(" ",nums));
+            
+        }
+    }
+}
