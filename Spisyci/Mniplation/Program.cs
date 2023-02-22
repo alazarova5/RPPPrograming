937e10effd1a2afb39b317ef4bb6f02aa58da7a3
alazarova5 <aleksandralazarova25@gmail.com>
2023-02-22 09:31:12 +0200

    5 zad?

---
+++ Spisyci/Mniplation/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mniplation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+                string cmd = input[0];
+                if (cmd == "print")
+                {
+                    break;
+                }
+
+                switch (cmd)
+                {
+                    case "add":
+                        {
+                            var index = int.Parse(input[1]);
+                            var element = int.Parse(input[2]);
+                            nums.Insert(index, element);
+                            break;
+                        }
+
+                    case "addMany":
+                        {
+                            break;
+                        }
+
+
+                    case "contains":
+                        {
+                            break;
+                        }
+
+                    case "remove":
+                        {
+                            break;
+                        }
+
+                    case "shift":
+                        {
+                            break;
+                        }
+
+                    case "sumPairs":
+                        {
+                            break;
+                        }
+
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
