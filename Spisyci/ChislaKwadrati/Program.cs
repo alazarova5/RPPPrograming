2b2ddb95f3e43278f36f99ca3a07e8e204898c80
alazarova5 <aleksandralazarova25@gmail.com>
2023-02-22 08:41:32 +0200

    adding files

---
+++ Spisyci/ChislaKwadrati/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChislaKwadrati
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
