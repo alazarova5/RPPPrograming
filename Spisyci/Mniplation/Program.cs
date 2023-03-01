1ae3562980aaa3fe465f64351ccaa89ddaee1dfc
alazarova5 <aleksandralazarova25@gmail.com>
2023-03-01 09:01:20 +0200

    zad 5

--- Spisyci/Mniplation/Program.cs
+++ Spisyci/Mniplation/Program.cs
@@ -30,27 +30,50 @@ namespace Mniplation
 
                     case "addMany":
                         {
+                            int index = int.Parse(input[1]);
+                            for (int i = input.Count() - 1; i >= 2; i--)
+                            {
+                                nums.Insert(index, int.Parse(input[i]));
+                            }
                             break;
                         }
 
 
                     case "contains":
                         {
+                            int element = int.Parse(input[1]);
+                            Console.WriteLine(nums.IndexOf(element));
                             break;
                         }
 
                     case "remove":
                         {
+                            int index = int.Parse(input[1]);
+                            nums.RemoveAt(index);
                             break;
                         }
 
                     case "shift":
                         {
+                            var pos = int.Parse(input[1]);
+                            for (int i = 0; i < pos%nums.Count(); i++)
+                            {
+                                int element = nums[0];
+                                nums.RemoveAt(0);
+                                nums.Add(element);
+                            }
+
                             break;
                         }
 
                     case "sumPairs":
                         {
+                            List<int> result = new List<int>();
+                            for (int i = 0; i < nums.Count; i=i+2)
+                            {
+                                result.Add(nums[i] + nums[i + 1]);
+                            }
+                            nums = result;
                             break;
                         }
 
