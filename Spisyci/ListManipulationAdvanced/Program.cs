1d41ef7e55a76ef2cc26d3502310e940f1912512
alazarova5 <aleksandralazarova25@gmail.com>
2023-03-06 09:04:27 +0200

    sweti w cherweno???

---
+++ Spisyci/ListManipulationAdvanced/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListManipulationAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
