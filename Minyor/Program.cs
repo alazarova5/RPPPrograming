385aa5779be1e36c697836ceb8899dde0a5c88a0
Sandy <124555898+alazarova5@users.noreply.github.com>
2023-03-15 08:45:36 +0200

    Minyor
    dict

---
+++ Minyor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Minyor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line=="stop")
+                {
+                    break;
+                }
+                int n = int.Parse(Console.ReadLine());
+                dict.Add(line, n);
+            }
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
