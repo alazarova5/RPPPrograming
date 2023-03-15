55897e487ce2cb22416f131363a67178a3b2cfaf
Sandy <124555898+alazarova5@users.noreply.github.com>
2023-03-15 08:32:19 +0200

    Add files via upload
    dict

---
+++ Dictionary/Minyor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Minyor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
