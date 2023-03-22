057f22435101a1f813777e139f4c37f485a3b2ae
alazarova5 <aleksandralazarova25@gmail.com>
2023-03-22 09:35:20 +0200

    arrays

---
+++ MultiDementionArrays/PrimaryDiagonal/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dimension = int.Parse(Console.ReadLine());
+            int rows = dimension;
+            int colums = dimension;
+            int[,] arr = new int[rows, colums];
+
+            //wywejdane
+            for (int i = 0; i < rows; i++)
+            {
+                int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int j = 0; j < colums; j++)
+                {
+                    arr[i, j] = input[j];
+                }
+            }
+
+            int sum = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                sum =sum + arr[i, i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ MultiDementionArrays/SymbolInMatrix/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dimension = int.Parse(Console.ReadLine());
+            int rows = dimension;
+            int colums = dimension;
+            char[,] arr = new char[rows, colums];
+
+            for (int i = 0; i < dimension; i++)
+            {
+                string input = Console.ReadLine();
+                for (int j = 0; j < input.Length; j++)
+                {
+                    arr[i, j] = input[j];
+                }
+            }
+
+            char nameriSimvol = char.Parse(Console.ReadLine());
+            bool tyrsene = false;
+
+            for (int i = 0; i < dimension; i++)
+            {
+                for (int j = 0; j < dimension; j++)
+                {
+                    if (arr[i, j] == nameriSimvol)
+                    {
+                        Console.WriteLine($"({i}, {j})");
+                        tyrsene = true;
+                        break;
+                    }
+                }
+            }
+            if (!tyrsene)
+            {
+                Console.WriteLine($"{nameriSimvol} does not occur in the matrix");
+            }
+        }
+    }
+}
