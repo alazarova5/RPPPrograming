2b65d98d90a287ddd73cdbd8fc0e2d69f97590cd
Sandy <124555898+alazarova5@users.noreply.github.com>
2023-03-29 09:11:53 +0300

    zad ot izpitwane

---
+++ Sandyy/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dim = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = dim[0];
+            int colums = dim[1];
+            int[,] arr = new int[rows, colums];
+
+            for (int i = 0; i < rows; i++)
+            {
+                int[] input = Console.ReadLine().Split(";").Select(int.Parse).ToArray();
+                for (int j = 0; j <  colums; j++)
+                {
+                    arr[i, j] = input[j];
+                }
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    if (arr[i,j]>20)
+                    {
+                        Console.WriteLine(arr[i, j]);
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ Sandyy/Sandyy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sandyy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> d = new Dictionary<string, int>();
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+                if (input[0] == "PRINT")
+                {
+                    break;
+                }
+                string name = input[0];
+                string city = input[1];
+
+                if (!d.ContainsKey(city))
+                {
+                    d.Add(city, 1);
+                }
+                else
+                {
+                    d[city] ++;
+                    
+                }
+            }
+            Console.WriteLine($"Towns count: {d.Count()}");
+            foreach (var item in d)
+            {
+               Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
