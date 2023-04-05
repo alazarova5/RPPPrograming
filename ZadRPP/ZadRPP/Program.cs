8dca10d7530082036f4201ef3f29a4721ed44024
Sandy <124555898+alazarova5@users.noreply.github.com>
2023-04-05 08:58:43 +0300

    primerna zadacha

---
+++ ZadRPP/ZadRPP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ZadRPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> students = new Dictionary<string, int>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                string subject = input;
+                if (!students.ContainsKey(subject))
+                {
+                    students.Add(subject, 1);
+                }
+                else
+                {
+                    students[subject]++;
+                }
+            }
+        Console.WriteLine($"Subjects count: {students.Count()}");
+            foreach (var item in students)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            
+            }
+        }
+    }
+}
